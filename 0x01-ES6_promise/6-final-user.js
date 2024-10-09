d37622091f32a2c922e16825a70bf29e6793f3a4
Gloriana <141042662+Globski@users.noreply.github.com>
2024-10-09 16:07:13 -0700

    Implement handleProfileSignup to handle multiple promises and return results.

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,10 +2,12 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
+  const promises = [
     signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((results) => {
+    uploadPhoto(fileName)
+  ];
+
+  return Promise.allSettled(promises).then((results) => {
     return results.map((result) => ({
       status: result.status,
       value: result.status === 'fulfilled' ? result.value : result.reason,
