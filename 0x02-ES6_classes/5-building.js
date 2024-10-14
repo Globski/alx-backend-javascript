e19b85cbb394eda3d6ef8e99cfa20dc63077630b
Gloriana <141042662+Globski@users.noreply.github.com>
2024-10-14 03:52:44 -0700

    Implement abstract Building class with evacuationWarningMessage.

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,6 +1,10 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
+
+    if (new.target === Building) {
+      throw new Error('Cannot instantiate abstract class Building directly');
+    }
   }
 
   get sqft() {
