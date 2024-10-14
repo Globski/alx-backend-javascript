308f2074a5422c1e3091abf786b971d160586e6b
Gloriana <141042662+Globski@users.noreply.github.com>
2024-10-14 11:57:44 +0100

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,17 +1,13 @@
 export default class Building {
-  constructor(sqft) {
-    this._sqft = sqft;
+    constructor(sqft) {
+        this._sqft = sqft;
 
-    if (new.target === Building) {
-      throw new Error('Cannot instantiate abstract class Building directly');
+        if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+            throw new Error('Class extending Building must override evacuationWarningMessage');
+        }
     }
-  }
 
-  get sqft() {
-    return this._sqft;
-  }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
+    get sqft() {
+        return this._sqft;
+    }
 }
